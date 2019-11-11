2b4199ebba3209b39a5de76adc61713bf330a9af
Alexandru Balan <balan.alexandru1997@tutanota.com>
2019-11-11 15:14:15 +0200

    Added DropIfModelChanges to database

--- YahooGroups/YahooGroups/Global.asax.cs
+++ YahooGroups/YahooGroups/Global.asax.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using YahooGroups.Models;
 
 namespace YahooGroups
 {
@@ -16,6 +18,7 @@ namespace YahooGroups
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer<ApplicationDbContext>(new DropCreateDatabaseIfModelChanges<ApplicationDbContext>());
         }
     }
 }

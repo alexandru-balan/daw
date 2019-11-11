7b9eed10eceaddbf152ddcddf35b098ea48b9e9b
Alexandru Balan <balan.alexandru1997@tutanota.com>
2019-11-11 14:47:01 +0200

    Added IdentityFramework for authentication

--- YahooGroups/YahooGroups/Properties/AssemblyInfo.cs
+++ YahooGroups/YahooGroups/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("630c0e87-85d5-4906-8dd3-d54f5e030960")]
+[assembly: Guid("f9b84caf-fad4-4998-8643-bc2c384be8ad")]
 
 // Version information for an assembly consists of the following four values:
 //

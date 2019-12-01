58b79e4c99ba1af17d465aa269097205a1cd0e89
Alexandru Balan <balan.alexandru1997@tutanota.com>
2019-12-01 18:33:20 +0200

    User Roles
    This commit solves the first point on the list. (Add 4 user roles...)
    
    New:
    * Added roles for admin, unregistered, registered user and moderator
    * Added two admin users: alexandru.balan@fictiv.com and horatiu.razvan@fictiv.com (See Startup.cs for passwords)
    * RoleManager class that handles the creation of roles (see IdentityConfig.cs)
    
    Modified:
    * Upgraded Bootstrap

--- YahooGroups/YahooGroups/Startup.cs
+++ YahooGroups/YahooGroups/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using YahooGroups.Models;
 
 [assembly: OwinStartupAttribute(typeof(YahooGroups.Startup))]
 namespace YahooGroups
@@ -9,6 +12,74 @@ namespace YahooGroups
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            createAdminUsersAndAppRoles();
+        }
+
+        private void createAdminUsersAndAppRoles ()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            // Adaugam rolurile utilizatorilor in aplicatie
+
+            // Rolul de Administrator
+            if (!roleManager.RoleExists("admin"))
+            {
+                // Se adauga rolul de admin
+                var role = new IdentityRole();
+                role.Name = "admin";
+                roleManager.Create(role);
+
+                // Se adauga administratorii aplicatiei
+                var admin1 = new ApplicationUser();
+                var admin2 = new ApplicationUser();
+
+                admin1.UserName = "alexandru.balan@fictiv.com";
+                admin1.Email = "alexandru.balan@fictiv.com";
+
+                admin2.UserName = "horatiu.razvan@fictiv.com";
+                admin2.Email = "horatiu.razvan@fictiv.com";
+
+                var created = userManager.Create(admin1, "Admin1!"); // Parola super sigura
+                if (created.Succeeded)
+                {
+                    userManager.AddToRole(admin1.Id, "admin");
+                }
+
+                created = userManager.Create(admin2, "Admin2!"); // Parola super sigura
+                if (created.Succeeded)
+                {
+                    userManager.AddToRole(admin2.Id, "admin");
+                }
+            }
+
+            // Rolul de utilizator neinregistrat
+            if (!roleManager.RoleExists("unregistered"))
+            {
+                var role = new IdentityRole();
+                role.Name = "unregistered";
+                roleManager.Create(role);
+            }
+
+            // Rolul de utilizator inregistrat
+            if (!roleManager.RoleExists("user"))
+            {
+                var role = new IdentityRole();
+                role.Name = "user";
+                roleManager.Create(role);
+            }
+
+            // Rolul de moderator
+            if (!roleManager.RoleExists("moderator"))
+            {
+                var role = new IdentityRole();
+                role.Name = "moderator";
+                roleManager.Create(role);
+            }
         }
     }
 }
